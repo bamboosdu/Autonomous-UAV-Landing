2753b7f9b34333caa55fac4cffc5e3df0a7b40a8
bamboosdu <bamboosdu@gmail.com>
2020-10-14 16:21:57 +0800

    10.14

--- detect_board/build/CMakeFiles/marker.dir/cmake_clean.cmake
+++ detect_board/build/CMakeFiles/marker.dir/cmake_clean.cmake
@@ -1,8 +1,8 @@
 file(REMOVE_RECURSE
   "CMakeFiles/marker.dir/detect_multi_markers.cpp.o"
-  "CMakeFiles/marker.dir/include/Utils.cpp.o"
-  "marker"
+  "CMakeFiles/marker.dir/home/qiang/zq/git-space/drone_about/aruco_board/detect_board/include/Utils.cpp.o"
   "marker.pdb"
+  "marker"
 )
 
 # Per-language clean rules from dependency scanning.
